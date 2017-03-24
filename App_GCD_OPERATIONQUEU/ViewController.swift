a7c77c2c7fe45922ef9832f9769952536159e2c6
cice <cice@aula8PovMacN09.local>
2017-03-24 16:13:45 +0100

    Initial Commit

---
+++ App_GCD_OPERATIONQUEU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_GCD_OPERATIONQUEU
+//
+//  Created by cice on 24/3/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
